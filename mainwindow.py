7b13b85285445f0b75b7b920fae89f6cbcf31c13
jiafan <596137647@qq.com>
2019-03-13 11:22:30 +0800

    完整第一版

--- mainwindow.py
+++ mainwindow.py
@@ -5,80 +5,126 @@
 # Created by: PyQt5 UI code generator 5.11.3
 #
 # WARNING! All changes made in this file will be lost!
-
+from PyQt5.QtCore import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QTimer
 import paho.mqtt.client as mqtt
 import threading
 import thread
 
-
+"""
+窗体类
+"""
 class Ui_MainWindow(object):
     Qos_number = 0      #Qos质量
     IP_number = " "     #连接服务器的IP地址
     topic_content = " " #订阅主题名称
     display = " "       #返回内容
+    ClientThread = None       #MQTT客户端线程
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(632, 409)
         self.centralWidget = QtWidgets.QWidget(MainWindow)
+        #创建容器
         self.centralWidget.setObjectName("centralWidget")
+
         self.subscribe_box = QtWidgets.QGroupBox(self.centralWidget)
-        self.subscribe_box.setGeometry(QtCore.QRect(50, 100, 531, 201))
+        self.subscribe_box.setGeometry(QtCore.QRect(50, 50, 531, 301))
         self.subscribe_box.setObjectName("subscribe_box")
+
         self.server_IP = QtWidgets.QLineEdit(self.subscribe_box)
         self.server_IP.setGeometry(QtCore.QRect(100, 50, 113, 21))
         self.server_IP.setObjectName("server_IP")
+
         self.topic = QtWidgets.QLineEdit(self.subscribe_box)
         self.topic.setGeometry(QtCore.QRect(100, 80, 113, 21))
         self.topic.setObjectName("topic")
+
         self.label = QtWidgets.QLabel(self.subscribe_box)
-        self.label.setGeometry(QtCore.QRect(10, 50, 81, 16))
+        self.label.setGeometry(QtCore.QRect(20, 50, 81, 16))
         self.label.setObjectName("label")
+
         self.label_2 = QtWidgets.QLabel(self.subscribe_box)
         self.label_2.setGeometry(QtCore.QRect(20, 80, 60, 16))
         self.label_2.setObjectName("label_2")
+
         self.label_3 = QtWidgets.QLabel(self.subscribe_box)
-        self.label_3.setGeometry(QtCore.QRect(30, 110, 31, 16))
+        self.label_3.setGeometry(QtCore.QRect(20, 110, 31, 16))
         self.label_3.setObjectName("label_3")
+
         self.QoS = QtWidgets.QLineEdit(self.subscribe_box)
         self.QoS.setGeometry(QtCore.QRect(100, 110, 113, 21))
         self.QoS.setObjectName("QoS")
+
         self.subscribe = QtWidgets.QPushButton(self.subscribe_box)
-        self.subscribe.setGeometry(QtCore.QRect(70, 150, 101, 32))
+        self.subscribe.setGeometry(QtCore.QRect(10, 150, 101, 32))
         self.subscribe.setObjectName("subscribe")
+
+        self.unsubscribe = QtWidgets.QPushButton(self.subscribe_box)
+        self.unsubscribe.setGeometry(QtCore.QRect(120, 150, 101, 32))
+        self.unsubscribe.setObjectName("unsubscribe")
+
+        self.clear = QtWidgets.QPushButton(self.subscribe_box)
+        self.clear.setGeometry(QtCore.QRect(10, 180, 101, 32))
+        self.clear.setObjectName("clear")
+
         self.receive_box = QtWidgets.QTextBrowser(self.subscribe_box)
-        self.receive_box.setGeometry(QtCore.QRect(270, 40, 241, 141))
+        self.receive_box.setGeometry(QtCore.QRect(270, 40, 241, 241))
         self.receive_box.setObjectName("receive_box")
         MainWindow.setCentralWidget(self.centralWidget)
 
         self.retranslateUi(MainWindow)
         '''逻辑功能'''
-        self.subscribe.clicked.connect(self.subscribe_submit)
+        self.subscribe.clicked.connect(self.NewClient)
+        self.unsubscribe.clicked.connect(self.Unsubscribe)
+        self.clear.clicked.connect(self.Clear)
 
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.subscribe_box.setTitle(_translate("MainWindow", "订阅信息"))
-        self.label.setText(_translate("MainWindow", "服务器IP地址"))
-        self.label_2.setText(_translate("MainWindow", "订阅主题"))
+        self.subscribe_box.setTitle(_translate("MainWindow", "Client Interface"))
+        self.label.setText(_translate("MainWindow", "IP Adress"))
+        self.label_2.setText(_translate("MainWindow", "Sub Topic"))
         self.label_3.setText(_translate("MainWindow", "Qos"))
         self.subscribe.setText(_translate("MainWindow", "subscribe"))
+        self.unsubscribe.setText(_translate("MainWindow", "unsubscribe"))
+        self.clear.setText(_translate("MainWindow", "clear"))
+
+        self.server_IP.setText("120.78.172.153")
+        self.topic.setText("$SYS/#")
 
     '''按下subscribe后执行动作'''
-    def subscribe_submit(self):
-        self.receive_box.clear()
-        self.Qos_number = self.QoS.text()
-        self.IP_number = self.server_IP.text()
-        self.topic_content = self.topic.text()
-        print(self.Qos_number)
-        print(self.IP_number)
-        print(self.topic_content)
-        self.thread = thread.RunThread(IP_number=self.IP_number,topic_content=self.topic_content)
-        self.thread.signal.connect(self.refresh)
-        self.thread.start()
+    def NewClient(self):
+        try:
+            self.ClientThread.client.unsubscribe(self.topic_content) #再次点击先取消之前的订阅并订阅新主题
+            self.receive_box.clear()
+            self.topic_content = self.topic.text()
+            self.ClientThread.client.subscribe(self.topic_content)
+        except:
+            self.receive_box.clear()
+            self.Qos_number = self.QoS.text()
+            self.IP_number = self.server_IP.text()
+            self.topic_content = self.topic.text()
+            # print(self.Qos_number)
+            # print(self.IP_number)
+            # print(self.topic_content)
+            self.ClientThread = thread.Client_RunThread(IP_number=self.IP_number,topic_content=self.topic_content)
+            self.ClientThread.signal.connect(self.Refresh)    #将子线程的信号连接到主线程的刷新函数上
+            self.ClientThread.start()
 
-    def refresh(self,msg):
-        self.receive_box.append(msg)
+    def Unsubscribe(self):
+        try:
+            self.ClientThread.client.unsubscribe(self.topic_content)
+        except:
+            self.receive_box.append("当前未订阅任何主题")
+            self.receive_box.repaint()
+
+    def Refresh(self,msg):
+        self.receive_box.append(msg)    #将收到的消息显示在接受框中
+
+    def Clear(self):
+        self.receive_box.clear()
+        self.receive_box.repaint()
--- thread.py
+++ thread.py
@@ -3,40 +3,43 @@ import time
 import paho.mqtt.client as mqtt
 
 
-
-class RunThread(QThread):
-    signal = pyqtSignal(str)
+'''
+继承Qthread，实现在子线程中修改主线程UI，传入参数为服务器IP地址，订阅主题
+'''
+class Client_RunThread(QThread):
+    signal = pyqtSignal(str) #slot信号
     Qos_number = 0      #Qos质量
     IP_number = " "     #连接服务器的IP地址
     topic_content = " " #订阅主题名称
-    display = " "       #返回内容
+    client = mqtt.Client()  #客户端实例
+
     def __init__(self,IP_number,topic_content):
-        super(RunThread,self).__init__()
+        super(Client_RunThread,self).__init__()
         self.IP_number = IP_number
         self.topic_content = topic_content
 
+
+    '''override原run函数'''
     def run(self):
-        self.connect()
+        self.connect()  #创建mqtt线程
 
     '''创建pahomqtt实例，并尝试连接'''
     def connect(self):
-        client = mqtt.Client()
-        client.on_connect = self.on_connect
-        client.on_message = self.on_message
-        client.connect(self.IP_number, 1883, 60)
-        client.loop_forever()
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.connect(self.IP_number, 1883, 60)
+        self.client.loop_forever()
 
     '''连接状态回调函数'''
     def on_connect(self,client, userdata, flags, rc):
         print("Connected with result code " + str(rc))
         #client.subscribe("$SYS/#")
+        self.client = client
         client.subscribe(self.topic_content)
         self.signal.emit(rc)
 
 
     '''接受消息的回调函数'''
     def on_message(self,client, userdata, msg):
-        print(msg.topic + ":" + msg.payload.decode())
+        #print(msg.topic + ":" + msg.payload.decode())
         self.signal.emit(msg.topic + ":" + msg.payload.decode())
-
-
